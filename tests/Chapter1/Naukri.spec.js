1be0971931ae65de2ff8c491d5d0946c8bc7a326
sayhitosujith <sayhitosujith@gmail.com>
2025-07-23 21:37:22 +0530

    removed other browsers except chrome

--- tests/Chapter1/Naukri.spec.js
+++ tests/Chapter1/Naukri.spec.js
@@ -3,11 +3,14 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
 
   await page.goto('https://www.naukri.com/nlogin/login?utm_source=google&utm_medium=cpc&utm_campaign=Brand&gad_source=1&gclid=CjwKCAjwo6GyBhBwEiwAzQTmc34DfBd9dNPPn_R_W3UozmHxoGFxQRepNJgOcFPHLMUoYhEwNErtOxoC6a0QAvD_BwE&gclsrc=aw.ds');
+  
   await page.getByRole('textbox', { name: 'Enter Email ID / Username' }).fill('sayhitosujith@gmail.com');
   await page.getByRole('textbox', { name: 'Enter Password' }).fill('Qw@12345678');
   await page.getByRole('button', { name: 'Login', exact: true }).click();
-  
+
+  // ✅ Wait for "View profile" to be visible — ensures login is successful
   await expect(page.getByRole('link', { name: 'View profile' })).toBeVisible();
+
   await page.getByRole('link', { name: 'View profile' }).click();
   await page.getByRole('listitem').filter({ hasText: 'Resume headline' }).locator('span').click();
   await page.locator('#lazyResumeHead').getByText('editOneTheme').click();
