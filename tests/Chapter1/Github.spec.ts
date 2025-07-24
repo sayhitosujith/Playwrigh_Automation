29c5933395c2afbb863fe5ed811a2502182c0e7d
Sadashivareddys <sujithreddy1991@gmail.com>
2025-07-24 15:03:24 +0530

    added till logout

--- tests/Chapter1/Github.spec.ts
+++ tests/Chapter1/Github.spec.ts
@@ -8,5 +8,9 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('WelcometoRc@1');
   await page.getByRole('button', { name: 'Sign in', exact: true }).click();await page.goto('https://github.com/');
-  await expect(page.locator('#contextregion-basiccrumb-dashboard-link')).toContainText('Dashboard');
+  await expect(page.locator('#contextregion-basiccrumb-dashboard-link')).toContainText('Dashboard');await page.goto('https://github.com/');
+  await page.getByRole('button', { name: 'Open user navigation menu' }).click();
+  await page.getByRole('link', { name: 'Sign out' }).click();
+  await expect(page.getByRole('main')).toContainText('Sign out from all accounts');
+  await expect(page.getByRole('main')).toContainText('Sign out from all accounts');
 });
