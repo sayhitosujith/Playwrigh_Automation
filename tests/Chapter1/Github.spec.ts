6b79a68701faadc84204bcc85b65c899194a3681
Sadashivareddys <sujithreddy1991@gmail.com>
2025-07-24 14:41:42 +0530

    added assertions

--- tests/Chapter1/Github.spec.ts
+++ tests/Chapter1/Github.spec.ts
@@ -6,7 +6,7 @@ test('test', async ({ page }) => {
   await page.goto('https://github.com/login');
   await page.getByRole('textbox', { name: 'Username or email address' }).fill('sayhitosujith@gmail.com');
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Qw@12345678');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+  await page.getByRole('textbox', { name: 'Password' }).fill('WelcometoRc@1');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();await page.goto('https://github.com/');
+  await expect(page.locator('#contextregion-basiccrumb-dashboard-link')).toContainText('Dashboard');
 });
