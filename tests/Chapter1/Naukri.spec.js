af63a3c45f47ffa5d26895a31d36bff82ec550d2
Sadashivareddys <sujithreddy1991@gmail.com>
2025-07-27 22:05:54 +0530

    removed commented code

--- tests/Chapter1/Naukri.spec.js
+++ tests/Chapter1/Naukri.spec.js
@@ -1,4 +1,4 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.naukri.com/');
