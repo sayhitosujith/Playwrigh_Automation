1130da217d440fffcff57fa534259fab7496fbfe
Sadashivareddys <sujithreddy1991@gmail.com>
2025-07-28 15:03:03 +0530

    added valid url

--- tests/API_Tests/01_POST_API_Request_Static.spec.ts
+++ tests/API_Tests/01_POST_API_Request_Static.spec.ts
@@ -1,13 +1,14 @@
-import { test, expect, request as playwrightRequest } from '@playwright/test';
+import { test } from '@playwright/test';
 import postAPIRequest from '../../data/POST_API_Request.json';
 
-test('create POST API Request using static file in playwright and Typescript', async () => {
-  const apiContext = await playwrightRequest.newContext({
-    baseURL: process.env.BASE_API_URL, // Make sure this is defined
-  });
+test.use({
+    baseURL: process.env.BASE_API_URL || 'https://restful-booker.herokuapp.com',
+})
 
-  const postAPIResponse = await apiContext.post('/booking', { data: postAPIRequest });
+test('create POST API Request using static file in playwright and Typescript', async ({ request }) => {
+
+  const postAPIResponse = await request.post('/booking', {data: postAPIRequest})
+
   const jsonPOSTAPIResponse = await postAPIResponse.json();
-
-  console.log('POST API Response :' + JSON.stringify(jsonPOSTAPIResponse, null, 2));
+    console.log('POST API Response :'+JSON.stringify(jsonPOSTAPIResponse, null, 2));
 });
