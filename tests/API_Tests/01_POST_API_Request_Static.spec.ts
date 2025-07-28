ad4d7f64a132caa9babadbb1a07f7db6ae579294
Sadashivareddys <sujithreddy1991@gmail.com>
2025-07-28 15:30:38 +0530

    added more assertions

--- tests/API_Tests/01_POST_API_Request_Static.spec.ts
+++ tests/API_Tests/01_POST_API_Request_Static.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import postAPIRequest from '../../data/POST_API_Request.json';
 
 test.use({
@@ -6,9 +6,25 @@ test.use({
 })
 
 test('create POST API Request using static file in playwright and Typescript', async ({ request }) => {
-
+//create POST API request
   const postAPIResponse = await request.post('/booking', {data: postAPIRequest})
 
+  //print JSON API response
   const jsonPOSTAPIResponse = await postAPIResponse.json();
     console.log('POST API Response :'+JSON.stringify(jsonPOSTAPIResponse, null, 2));
+
+    //validation of POST API response
+    expect(postAPIResponse.status()).toBe(200);
+    expect(postAPIResponse.statusText()).toBe('OK');
+    expect(postAPIResponse.headers()['content-type']).toContain('application/json; charset=utf-8');
+
+    //validate property/key names
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('firstname');
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('lastname');
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('totalprice');
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('depositpaid');
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('bookingdates');
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('additionalneeds');
+    expect(jsonPOSTAPIResponse.booking.bookingdates).toHaveProperty('checkin');
+    expect(jsonPOSTAPIResponse.booking.bookingdates).toHaveProperty('checkout');
 });
