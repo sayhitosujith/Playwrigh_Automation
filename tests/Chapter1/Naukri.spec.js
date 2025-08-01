777af2820808aabe89c38f7a28267187e5244d92
Sadashivareddys <sujithreddy1991@gmail.com>
2025-08-01 11:28:48 +0530

    reverted

--- tests/Chapter1/Naukri.spec.js
+++ tests/Chapter1/Naukri.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Login to Naukri', async ({ page }) => {
   await page.goto('https://www.naukri.com/', { waitUntil: 'domcontentloaded' });
   const loginLink = page.getByRole('link', { name: 'Login', exact: true });
+  await expect(loginLink).toBeVisible(); // ensures it's available
   await loginLink.click();
 
   await page.getByRole('textbox', { name: /Email ID/i }).fill('sayhitosujith@gmail.com');
