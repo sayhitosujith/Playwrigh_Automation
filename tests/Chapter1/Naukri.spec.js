eaf8579bd032c74d8dd7631375ceddf0313d2423
Sadashivareddys <sujithreddy1991@gmail.com>
2025-08-01 11:35:55 +0530

    added stable test

--- tests/Chapter1/Naukri.spec.js
+++ tests/Chapter1/Naukri.spec.js
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('Login to Naukri', async ({ page }) => {
   await page.goto('https://www.naukri.com/', { waitUntil: 'domcontentloaded' });
-  const loginLink = page.getByRole('link', { name: 'Login', exact: true });
-  await expect(loginLink).toBeVisible(); // ensures it's available
-  await loginLink.click();
+  await expect(page.locator('#login_Layer')).toContainText('Login');
+  await page.getByRole('link', { name: 'Login', exact: true }).click();
+
 
   await page.getByRole('textbox', { name: /Email ID/i }).fill('sayhitosujith@gmail.com');
   await page.getByRole('textbox', { name: /password/i }).fill('yourPasswordHere');
