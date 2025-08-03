1dcf722eff47dd7a9e6b9dc270d9b7eb61a7ef85
Sadashivareddys <sujithreddy1991@gmail.com>
2025-08-03 12:06:24 +0530

    vidhya

--- tests/Chapter1/Naukri.spec.js
+++ tests/Chapter1/Naukri.spec.js
@@ -3,9 +3,11 @@ import { test, expect } from '@playwright/test';
 test('Login to Naukri', async ({ page }) => {
   await page.goto('https://www.naukri.com/', { waitUntil: 'domcontentloaded' });
   await expect(page.locator('#login_Layer')).toContainText('Login');
+  
+  //click on Login button
   await page.getByRole('link', { name: 'Login', exact: true }).click();
 
-
-  await page.getByRole('textbox', { name: /Email ID/i }).fill('sayhitosujith@gmail.com');
-  await page.getByRole('textbox', { name: /password/i }).fill('yourPasswordHere');
+//enter email and password
+  await page.getByRole('textbox', { name: /Email ID/i }).fill('vidhyaln95@gmail.com');
+  await page.getByRole('textbox', { name: /password/i }).fill('Qw@12345678');
 });
